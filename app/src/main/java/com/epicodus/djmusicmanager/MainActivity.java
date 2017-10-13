9dddd328179b0e2dfd42e6117fcfb5e2213a2e15
Esti Shay <esti.r.shay@gmail.com>
2017-10-13 12:02:08 -0700

    display dummy list of songs on Main activity

--- app/src/main/java/com/epicodus/djmusicmanager/MainActivity.java
+++ app/src/main/java/com/epicodus/djmusicmanager/MainActivity.java
@@ -4,20 +4,32 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-    @Bind(R.id.aboutButton)
-    Button mAboutButton;
+    @Bind(R.id.aboutButton) Button mAboutButton;
+    @Bind(R.id.listView) ListView mListView;
+    private String[] songs = new String[] {"The Lady is a Tramp by Ella Fitzgerald", "Fine Brown " +
+            "Frame by Lou Rawls", "Leaving on a Jet Plane by HB Radke and the Jet City Swingers",
+   "Blue Suit Boogie by Indigo Swing", "Scratching Circles by JD McPherson", "On Revival Day by " +
+            "Laverne Baker", "Lavender Coffin by Lionel Hampton and His Orchestra", "Santa Maria " +
+            "(Del Buen Ayre) by Gotan Project", "Home by Marc Brussard", "Cold Turkey by Anthony " +
+            "David", "Sweet Little Angel by Big Mama Thornton", "Bump and Grind by Jimmy " +
+            "Thackery", "John the Revelator by Govt Mule", "John the Revelator by Lee Roy Parnell"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, songs);
+        mListView.setAdapter(adapter);
 
         mAboutButton.setOnClickListener(new View.OnClickListener(){
             @Override
