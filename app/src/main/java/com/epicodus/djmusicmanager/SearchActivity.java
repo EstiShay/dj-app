507dd35db909b8787b3ba70db7593e208438de6a
Esti Shay <esti.r.shay@gmail.com>
2017-10-24 14:08:46 -0700

    add form validation to SearchActivity

--- app/src/main/java/com/epicodus/djmusicmanager/SearchActivity.java
+++ app/src/main/java/com/epicodus/djmusicmanager/SearchActivity.java
@@ -3,14 +3,20 @@ package com.epicodus.djmusicmanager;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class SearchActivity extends AppCompatActivity {
-    @Bind(R.id.searchTitleTextView)
-    TextView mSearchTitleTextView;
+    @Bind(R.id.searchTitleTextView) TextView mSearchTitleTextView;
+    @Bind(R.id.songTitleEditText) EditText mSongTitleEditText;
+    @Bind(R.id.artistNameEditText) EditText mArtistNameEditText;
+    @Bind(R.id.searchAPIButton) Button mSearchAPIButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,20 @@ public class SearchActivity extends AppCompatActivity {
 
         Typeface boolackFont = Typeface.createFromAsset(getAssets(), "fonts/Boolack.ttf");
         mSearchTitleTextView.setTypeface(boolackFont);
+
+        mSearchAPIButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (v == mSearchAPIButton) {
+                    String title = mSongTitleEditText.getText().toString();
+                    String artist = mArtistNameEditText.getText().toString();
+
+                    if (title.equals("")){
+                        Toast.makeText(SearchActivity.this, "Please enter song title", Toast.LENGTH_LONG).show();
+                    }
+                }
+
+            }
+        });
     }
 }
