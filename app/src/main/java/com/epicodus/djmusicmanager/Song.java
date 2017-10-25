83ca737e399a28469a1ac67686a6fd6640fd1571
Esti Shay <esti.r.shay@gmail.com>
2017-10-25 08:37:13 -0700

    create Song constructor

---
+++ app/src/main/java/com/epicodus/djmusicmanager/Song.java
@@ -0,0 +1,33 @@
+package com.epicodus.djmusicmanager;
+
+
+public class Song {
+
+    private String title;
+    private String artist;
+    private String album;
+    private String year;
+
+    public Song(String title, String artist, String album, String year){
+        this.title = title;
+        this.artist = artist;
+        this.album = album;
+        this.year = year;
+    }
+
+    public String getTitle(){
+        return title;
+    }
+
+    public String getArtist(){
+        return artist;
+    }
+
+    public String getAlbum(){
+        return album;
+    }
+
+    public String getYear(){
+        return year;
+    }
+}
