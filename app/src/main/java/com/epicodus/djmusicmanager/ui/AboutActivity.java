e89da6eeef739f1c0d1c2028840ffb9294b831e7
Esti Shay <esti.r.shay@gmail.com>
2017-10-26 14:37:08 -0700

    improve AboutActivity layout and text

--- app/src/main/java/com/epicodus/djmusicmanager/ui/AboutActivity.java
+++ app/src/main/java/com/epicodus/djmusicmanager/ui/AboutActivity.java
@@ -1,18 +1,27 @@
 package com.epicodus.djmusicmanager.ui;
 
+import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.epicodus.djmusicmanager.R;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class AboutActivity extends AppCompatActivity {
+public class AboutActivity extends AppCompatActivity implements View.OnClickListener {
     @Bind(R.id.aboutTitleTextView) TextView mAboutTitleTextView;
     @Bind(R.id.aboutTextView) TextView mAboutTextView;
+    @Bind(R.id.featuresTextView) TextView mFeaturesTextView;
+
+    @Bind(R.id.aboutButton) Button mAboutButton;
+    @Bind(R.id.searchButton) Button mSearchButton;
+    @Bind(R.id.addButton) Button mAddButton;
 
 
     @Override
@@ -26,5 +35,23 @@ public class AboutActivity extends AppCompatActivity {
         mAboutTitleTextView.setTypeface(boolackFont);
         Typeface PTCFont = Typeface.createFromAsset(getAssets(), "fonts/PTC55F.ttf");
         mAboutTextView.setTypeface(PTCFont);
+        mFeaturesTextView.setTypeface(PTCFont);
+
+        mAboutButton.setOnClickListener(this);
+        mSearchButton.setOnClickListener(this);
+        mAddButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mAboutButton){
+            Intent intent = new Intent(AboutActivity.this, AboutActivity.class);
+            startActivity(intent);
+        } else if (v == mSearchButton) {
+            Intent intent = new Intent(AboutActivity.this, SearchActivity.class);
+            startActivity(intent);
+        } else if (v == mAddButton) {
+            Toast.makeText(AboutActivity.this, "Feature coming soon!", Toast.LENGTH_LONG).show();
+        }
     }
 }
