965cb45d463040d486c455468f01b2c77ef74057
Esti Shay <esti.r.shay@gmail.com>
2017-10-26 20:55:39 -0700

    backup prelim database setup

---
+++ app/src/main/java/com/epicodus/djmusicmanager/ui/RecordFormActivity.java
@@ -0,0 +1,15 @@
+package com.epicodus.djmusicmanager.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.epicodus.djmusicmanager.R;
+
+public class RecordFormActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_form);
+    }
+}
