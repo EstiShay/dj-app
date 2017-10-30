00830360036dc5f214af0b1cdb3af14fc0891548
Esti Shay <esti.r.shay@gmail.com>
2017-10-30 12:21:16 -0700

    replace dummy data on MainActivity with saved records from Firebase

--- app/src/main/java/com/epicodus/djmusicmanager/adapters/FirebaseRecordViewHolder.java
+++ app/src/main/java/com/epicodus/djmusicmanager/adapters/FirebaseRecordViewHolder.java
@@ -8,7 +8,7 @@ import android.widget.TextView;
 import com.epicodus.djmusicmanager.R;
 import com.epicodus.djmusicmanager.models.Record;
 
-public class FirebaseRecordViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+public class FirebaseRecordViewHolder extends RecyclerView.ViewHolder{
     View mView;
     Context mContext;
 
@@ -16,11 +16,22 @@ public class FirebaseRecordViewHolder extends RecyclerView.ViewHolder implements
         super(songView);
         mView = songView;
         mContext = songView.getContext();
-        songView.setOnClickListener(this);
     }
 
     public void bindRecord(Record record) {
-        TextView titleTextView = (TextView)mView.findViewById(R.id.songTitleTextView);
-        TextView artistTextView = (TextView)mView.findViewById(R.id.artistNameTextView);
+        TextView titleArtistTextView = (TextView)mView.findViewById(R.id.songTitleArtistTextView);
+        TextView albumYearTextView = (TextView)mView.findViewById(R.id.albumYearTextView);
+
+        titleArtistTextView.setText(record.getRecTitle() + " by " + record.getRecArtist());
+        if (!record.getRecAlbum().equals("") && !record.getRecYear().equals("")){
+            albumYearTextView.setText(record.getRecAlbum() + " released " + record.getRecYear());
+        } else if (record.getRecYear().equals("")){
+            albumYearTextView.setText(record.getRecAlbum());
+        } else if (record.getRecAlbum().equals("")){
+            albumYearTextView.setText("Released " + record.getRecYear());
+        }
+
     }
+
+
 }
