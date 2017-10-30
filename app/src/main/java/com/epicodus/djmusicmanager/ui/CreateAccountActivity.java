2fc07fddf5bf17f78ae7dfc02dc59479622e9f11
Esti Shay <esti.r.shay@gmail.com>
2017-10-30 15:41:52 -0700

    create user accounts in Firebase

--- app/src/main/java/com/epicodus/djmusicmanager/ui/CreateAccountActivity.java
+++ app/src/main/java/com/epicodus/djmusicmanager/ui/CreateAccountActivity.java
@@ -2,14 +2,21 @@ package com.epicodus.djmusicmanager.ui;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.epicodus.djmusicmanager.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -22,6 +29,10 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
     @Bind(R.id.confirmPasswordEditText) EditText mConfirmPasswordEditText;
     @Bind(R.id.createAccountButton) Button mCreateAccountButton;
     @Bind(R.id.loginTextView) TextView mLoginTextView;
+
+    public static final String TAG = CreateAccountActivity.class.getSimpleName();
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,6 +47,11 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
         mEmailEditText.setTypeface(PTCFont);
         mPasswordEditText.setTypeface(PTCFont);
         mConfirmPasswordEditText.setTypeface(PTCFont);
+
+        mLoginTextView.setOnClickListener(this);
+        mCreateAccountButton.setOnClickListener(this);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -46,7 +62,26 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
             startActivity(intent);
             finish();
         } else if (v == mCreateAccountButton) {
-            //createNewAccount();
+            createNewAccount();
         }
+    }
+
+    private void createNewAccount(){
+        final String name = mNameEditText.getText().toString().trim();
+        final String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        String confirmPassword = mConfirmPasswordEditText.toString().trim();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Log.d(TAG, "Authentication successful");
+                        } else {
+                            Toast.makeText(CreateAccountActivity.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
