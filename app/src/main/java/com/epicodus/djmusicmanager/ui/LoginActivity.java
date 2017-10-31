df03612f12dd0320c08cfdcb7986ee8975180dcb
Esti Shay <esti.r.shay@gmail.com>
2017-10-31 10:18:38 -0700

    log user in using Firebase auth

--- app/src/main/java/com/epicodus/djmusicmanager/ui/LoginActivity.java
+++ app/src/main/java/com/epicodus/djmusicmanager/ui/LoginActivity.java
@@ -2,30 +2,42 @@ package com.epicodus.djmusicmanager.ui;
 
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.epicodus.djmusicmanager.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+    public static final String TAG = LoginActivity.class.getSimpleName();
     @Bind(R.id.registerTextView) TextView mRegisterTextView;
     @Bind(R.id.appNameTextView) TextView mAppNameTextView;
     @Bind(R.id.subtitleTextView) TextView mSubtitleTextView;
     @Bind(R.id.emailEditText) TextView mEmailEditText;
     @Bind(R.id.passwordEditText) TextView mPasswordEditText;
+    @Bind(R.id.loginButton) Button mLoginButton;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
         ButterKnife.bind(this);
         mRegisterTextView.setOnClickListener(this);
+        mLoginButton.setOnClickListener(this);
 
         Typeface boolackFont = Typeface.createFromAsset(getAssets(), "fonts/Boolack.ttf");
         mAppNameTextView.setTypeface(boolackFont);
@@ -33,6 +45,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         mSubtitleTextView.setTypeface(PTCFont);
         mEmailEditText.setTypeface(PTCFont);
         mPasswordEditText.setTypeface(PTCFont);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     @Override
@@ -41,6 +55,36 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
             startActivity(intent);
             finish();
+        } else if (view == mLoginButton){
+            loginWithPassword();
         }
+    }
+
+    private void loginWithPassword(){
+        String email = mEmailEditText.getText().toString().trim();
+        String password = mPasswordEditText.getText().toString().trim();
+        if (email.equals("")){
+            mEmailEditText.requestFocus();
+            mEmailEditText.setError("Please enter your email");
+            return;
+        }
+        if (password.equals("")){
+            mPasswordEditText.requestFocus();
+            mPasswordEditText.setError("Password is required");
+            return;
+        }
+
+        mAuth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                    if (!task.isSuccessful()) {
+                        Log.w(TAG, "signInWithEmail", task.getException());
+                        Toast.makeText(LoginActivity.this, "Auth failed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
     }
 }
