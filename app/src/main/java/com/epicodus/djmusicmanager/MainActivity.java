cb9a9435588f73bac22b128b80dbc8acf70eda2a
Esti Shay <esti.r.shay@gmail.com>
2017-10-31 12:10:21 -0700

    retain username and display on MainActivity

--- app/src/main/java/com/epicodus/djmusicmanager/MainActivity.java
+++ app/src/main/java/com/epicodus/djmusicmanager/MainActivity.java
@@ -2,6 +2,7 @@ package com.epicodus.djmusicmanager;
 import android.app.FragmentManager;
 import android.content.Intent;
 import android.graphics.Typeface;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -24,6 +25,7 @@ import com.epicodus.djmusicmanager.ui.RecordFormDialogFragment;
 import com.epicodus.djmusicmanager.ui.SearchActivity;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -36,15 +38,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Bind(R.id.addButton) Button mAddButton;
     @Bind(R.id.welcomeTextView) TextView mWelcomeTextView;
     @Bind(R.id.subtitleTextView) TextView mSubtitleTextView;
-
-
     private DatabaseReference mTrackReference;
     private FirebaseRecyclerAdapter mFirebaseAdapter;
     @Bind(R.id.tracksRecyclerView) RecyclerView mTracksRecyclerview;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
@@ -54,12 +55,39 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Typeface PTCFont = Typeface.createFromAsset(getAssets(), "fonts/PTC55F.ttf");
         mSubtitleTextView.setTypeface(PTCFont);
 
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener(){
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth){
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null){
+                    mWelcomeTextView.setText("Welcome " + user.getDisplayName() + "!");
+                } else {
+
+                }
+            }
+        };
+
         mTrackReference = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_SONGS);
         setUpFirebaseAdapter();
 
         mAboutButton.setOnClickListener(this);
         mSearchButton.setOnClickListener(this);
         mAddButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
     }
 
     @Override
